6c9dea4da7f148fc00550ce4244b87766b20b8b8
andigans <74663590+andi-arch1@users.noreply.github.com>
2025-11-06 16:35:27 +0700

    Update send_emails.py

--- send_emails.py
+++ send_emails.py
@@ -1,23 +1,24 @@
 import smtplib
+import os
 from email.mime.text import MIMEText
-from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def send_email_notification(receiver_email, subject, body):
-    from dotenv import load_dotenv
-    import os
-
-    load_dotenv()
     sender_email = os.getenv("EMAIL_SENDER")
     sender_password = os.getenv("EMAIL_PASSWORD")
 
     msg = MIMEText(body, "html")
-    msg["Subject"] = Testing Notification
+    msg["Subject"] = subject
     msg["From"] = sender_email
     msg["To"] = receiver_email
 
-    with smtplib.SMTP(os.getenv("SMTP_SERVER"), int(os.getenv("SMTP_PORT"))) as server:
-        server.starttls()
-        server.login(sender_email, sender_password)
-        server.send_message(msg)
-
-    print(f"✅ Email sent to {receiver_email}")
+    try:
+        with smtplib.SMTP(os.getenv("SMTP_SERVER"), int(os.getenv("SMTP_PORT"))) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.send_message(msg)
+        print(f"✅ Email sent to {receiver_email}")
+    except Exception as e:
+        print(f"❌ Failed to send email: {e}")
